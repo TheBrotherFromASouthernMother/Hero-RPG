112e449e937d1d0d6cdee78a4bfdb53930897202
Christian Lowe <christglowe@gmail.com>
2018-02-14 12:00:25 -0600

    Added comments on our plan to create the version 7 of the RPG

--- hero_rpg.py
+++ hero_rpg.py
@@ -6,12 +6,76 @@
 # 2. do nothing - in which case the goblin will attack him anyway
 # 3. flee
 
+#TODOS
+              #CHARACTERS
+#1. MAKE HERO DO DOUBLE DAMAGE WITH A PROBABLITY OF 20%
+  #Using random module to generate percentage chance
+
+#2. MAKE A NEW CHARACTER MEDIC WHO CAN RECUPERATE HEALTH AFTER BEING ATTACKED
+#WITH A PROBABILITY OF 20%
+   #use random.randint method to do this
+
+#3 MAKE NEW CHARACTER SHADOW WHO HAS 1 HEALTH AND CAN ONLY TAKE DAMAGE
+#  EVERY TENTH TIME
+  #use counter variable to determine how many times shadow has been attacked,
+  #if counter variable is equal ten he takes damage, counter variable is reset to 0 if 
+  #he survives the attack
+
+#4 MAKE A ZOMBIE CHARACTER WHO CANNOT BE KILLED EVEN IF HIS HEALTH IS BELOW 0
+  #ZOMBIE CHARACTER HEALTH RETURN TRUE EVEN IF HEALTH IS LESS THAN 0
+
+#5 MAKE TWO UNIQUE CHARACTERS AND IMPLEMENT THEM
+  #ANTI-HERO - ANDY'S IDEA
+  #RANDOM GUARADIAN ANGEL - CHRISTIAN'S IDEA
+
+#6 MAKE CHARACTES GOBLIN PRIZE WOULD BE EQUAL TO 5, WIZARD TO 6
+  #INVENTORY ATTRITUBE WITH PROPERTIES INCLUDING PURSE ATTRIBUTE WHICH WOULD CONTAIN COINS
+  #IF ALIVE METHOD RETURNS FALSE ENEMY GETS THE VALUE OF CHARACTER'S PURSE
+
+
+              #ITEMS
+
+  #MAKE USE ITEM FUNCTION
+    #IF ITEM QUANITY IS GREATER THAN 0
+      #USE ITEM FUNCTION TAKE EFFECT
+      #BREAK
+    #ELIF THEY GO BACK
+      #IT TAKES THEM BACK OUT TO MAIN MENU
+      #PRINT YOU WASTED YOUR TURN
+    #ELSE
+      #PRINT SORRY ALL OUT OF ITEM.NAME
+      #RELIST THE INVENTORY PROBABLY USING A WHILE LOOP
+
+#1. MAKE HEALING POTION, RESTORE 10 HP
+  #
+
+
+
+
+#SUB MENU ON MAIN MENU OPTION 4
+    
+    #IF CHARACTER SELECTS SUB MENU IT SHOWS THEM THEIR INVENTORY AND OPTIONS
+      #
+
+#class Merchandise
+  #if not in battle
+  #display options 
+    #do_shoppig
+    #list_inventory
+  #else:
+  #list_inventory
+   #use_items
+
+
+
 def main():
   class Character:
     def __init__(self, name, health, power):
       self.name = name
       self.health = health
       self.power = power
+      #self.armour = armour
+      #self.evade = chance to evade
 
     def alive(self):
       return self.health > 0
@@ -20,10 +84,17 @@ def main():
       print("The {} has {} health and {} power.".format(self.name, self.health, self.power))
 
     def attack(self, enemy):
-      enemy.health -= self.power
+    
+      #if enemy.evade():
+        #print {self.name}'s attack has missed the {enemy}
+        #return False
+      enemy.health -= self.power #-  enemy.armour attribute
       print("The {} does {} damage to the {}.".format(self.name, self.power, enemy))
       if enemy.alive():
           print("The {} is dead.".format(enemy))   
+    
+    #def evade():
+      #random.radint() > self.evade percentage, return True else Return False
 
     def __str__(self):
       return self.name
@@ -37,6 +108,7 @@ def main():
   goblin = Character('goblin', 6, 2)
 
   while goblin.alive() and hero.alive():
+    #BATTFIELD MENU FUNCTION:
       hero.print_status()
       goblin.print_status()
       print()
@@ -54,6 +126,12 @@ def main():
       elif raw_input == "3":
         print("Goodbye.")
         break
+      #ELIF OPTION 4:
+        # GO TO INVENTORY SUB MENU
+          #OPTIONS
+            #SO ATTACK "3X ITEM" IMMEDIATELY CALLS HERO.ATTACK 3 TIMES"
+            #HEALING POTION WOULD RESTORE HEALTH IF QUANITY IS GREATER THAN 0
+
       else:
         print("Invalid input {}".format(raw_input))
 
@@ -63,3 +141,25 @@ def main():
 
 
 main()
+
+
+#if __name__ == "__main__":
+
+
+
+  #enemies = ['goblin, wizard, assasin, theif, crab] <--- all enemies 
+  #defeatedEnemies = [goblin]
+
+  #if len(deafetedEnemies) == len(enemies)
+    #print YOU DID IT YOU WON
+    #exit(0)
+
+  #enemy = enemies[random.randint()]
+  #if enemy is not in defeatedEnemies 
+    #pass
+  #elif enemy in deafetedEnemies gonb
+    #enemy = enemies[random.randint()]
+
+  
+  
+    
